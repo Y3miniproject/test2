71857accf353b1d5815192f0700761739cfad5de
hayeong-yu <seongtan2020@gmail.com>
2023-04-21 17:50:40 +0900

    second

--- src/testpakage/Application.java
+++ src/testpakage/Application.java
@@ -4,8 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("dddddd");
-		System.out.println("hy...first");
-		System.out.println("hy...first");
+		System.out.println("내용수정");
 
 	}
 
